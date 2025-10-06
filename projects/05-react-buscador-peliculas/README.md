3855e091c2a69184f1e297784d3d82675cd8da17
Ivan-S93 <samudioivan93@gmail.com>
2025-10-06 09:36:43 -0300

    Avanzando con el buscador de peliculas

--- projects/05-react-buscador-peliculas/README.md
+++ projects/05-react-buscador-peliculas/README.md
@@ -4,6 +4,7 @@ Enunciado
 Crea una aplicación para buscar películas
 
 API a usar: - https://www.omdbapi.com/ Consigue la API Key en la propia página web registrando tu email.
+API_KEY: 4287ad07
 
 Requerimientos:
 
--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,11 +1,44 @@
 import './App.css'
+import responseMovies from './mocks/with-results.json'
+import withoutResults from './mocks/no-results.json'
 
 function App() {
+  const movies = responseMovies.Search
+  const hasMovies = movies?.length > 0
+
   return (
-   <div>
-    <form className='form'>
-      <input placeholder='Avengers, Star Wars, The Matrix ... ' />
-    </form>
+   <div className= 'page'>
+
+    <header>
+      <h1>Buscador de Peliculas</h1>
+      <form className='form'>
+        <input placeholder='Avengers, Star Wars, The Matrix ... ' />
+        <button type='submit'>Buscar</button> 
+      </form>
+    </header>
+
+    <main>
+      {
+        hasMovies
+          ? (
+            <ul>
+              {
+                movies.map(movie => (
+                  <li key={movie.imdbID}>
+                    <h3>{movie.Title} </h3>
+                    <p>{movie.Year}</p>
+                    <img src={movie.Poster} alt={movie.Title} />
+                  </li>
+                ))
+              } 
+            </ul>
+          )
+        :(
+          <p>No se encuentran Peliculas</p>
+        )
+      }
+
+    </main>  
    </div>
   )
 }
